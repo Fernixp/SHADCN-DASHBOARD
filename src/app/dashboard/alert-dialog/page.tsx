02b5b012c9dbed75ce4dbbd2075bf1083bd72ab0
Fernando Nina <Fernandonina611@gmail.com>
2024-07-04 12:18:05 -0400

    feat: :sparkles: agregado componente Alert-dialog con Manejo de State

---
+++ src/app/dashboard/alert-dialog/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+export default function AlertDialogDemo() {
+  const [openAlert, setOpenAlert] = useState(false);
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      <AlertDialog open={openAlert} onOpenChange={setOpenAlert}>
+        <AlertDialogTrigger asChild>
+          <Button variant="outline">Show Dialog</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      <Button variant="default" onClick={() => setOpenAlert(!openAlert)}>
+        Abrir Con State
+      </Button>
+    </div>
+  );
+}
