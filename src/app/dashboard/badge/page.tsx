371ee194fa751af4df8afaef05ab3d6f9161b083
Fernando Nina <Fernandonina611@gmail.com>
2024-07-05 15:12:00 -0400

    feat: :sparkles: agregado componente badge con prop Capitalize

---
+++ src/app/dashboard/badge/page.tsx
@@ -0,0 +1,15 @@
+import { Badge } from "@/components/ui/badge";
+
+export function BadgeDemo() {
+  return (
+    <div className="grid grid-cols-4 gap-4 text-">
+      <Badge variant={"default"}>Badge</Badge>
+      <Badge variant={"destructive"}>Badge</Badge>
+      <Badge variant={"outline"}>Badge</Badge>
+      <Badge capitalize variant={"info"}>holA</Badge>
+      <Badge capitalize variant={"success"} >munDo</Badge>
+    </div>
+  );
+}
+
+export default BadgeDemo;
